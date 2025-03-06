a96c83e1920c20f4a0d0afccfe481a48cb2583e0
MuhamadHaikalMuzaki <hmzmuzaki@gmail.com>
2025-03-06 20:56:04 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+// Mendefinisikan Variable
+$nama = "Haikal";
+$umur = 18;
+$bb = 55;
+
+echo "Nama saya adalah : " .$nama;
+echo "<br/>Umur : " .$umur. " Tahun ";
+echo "<br/>Berat Badan : " .$bb. " kg";
+?>
